65802fa4d6f2310fac99e8f37e7bc345a90b4dbb
Ramin Kosfeld <r.kosfeld@tu-bs.de>
2021-11-21 02:22:48 +0100

    Maybe if I change the root folder, gitignore ignores all the unimportant files..

---
+++ Assets/Scripts/Gameplay/Driver.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+ * Call order: 
+ * Initialize()
+ *      NextPhase() [several times until up to date, every call is a phase++]
+ *      UpdatePhase() [once every frame]
+ * 
+ * DoCleanUp() is called once. When reaching the last timestamp, or already before. 
+ */
+public abstract class Driver
+{
+    protected float[] timestamps; //In seconds, dependent on the song. 
+    protected int phase = -1;
+    protected bool cleanedUp = false;
+
+    //TODO: Some JSON loads in here
+    public Driver()
+    {
+        
+    }
+
+    public float GetInstantiationPrewarmTime()
+    {
+        return timestamps[0] - 0.7f;
+    }
+
+    public bool InstantiateIfTime(float time)
+    {
+        if (time >= GetInstantiationPrewarmTime())
+        {
+            Initialize();
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    //Returns true if this Driver can be deleted (was cleaned up)
+    public bool Update(float timestamp)
+    {
+        if (cleanedUp) return true;
+        while (timestamp >= timestamps[phase+1])
+        {
+            if (++phase >= timestamps.Length)
+            {
+                CleanUp();
+                return true;
+            } else
+            {
+                NextPhase();
+            }
+        }
+        if (phase >= 0)
+        {
+            UpdatePhase(Mathf.InverseLerp(timestamps[phase], timestamps[phase+1], timestamp));
+        }
+        return false;
+    }
+
+    //Called before UpdatePhase, and for every raise by 1 in the phase. 
+    //Also called before you get the first UpdatePhase call
+    //(So if we progress several phases, this call comes several times, and then UpdatePhase)
+    //phase already contains the new phase value. 
+    //When passing the last timestamp, CleanUp() is called instead, if it wasnt called before. 
+    protected abstract void NextPhase();
+
+    //a is a value between 0 and <1, depending on how far we are with our phase. 
+    protected abstract void UpdatePhase(float a);
+
+    protected abstract void DoCleanUp();
+
+    //If in doubt: Call this before you exit somewhere, it will clean up only 1 time anyway. 
+    public void CleanUp()
+    {
+        if (!cleanedUp)
+        {
+            DoCleanUp();
+            cleanedUp = true;
+        }
+    }
+
+    //Called before any NextPhase comes. Used to allocate storage etc a short time before this driver actually has its first event.
+    //Once this is called, this driver is in the loop and receives updates with the timestamp
+    protected abstract void Initialize();
+}
---
+++ Assets/Scripts/Gameplay/MapManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * Actually this is rather the DriverManager. Creates them, updates them, keeps track of them. 
+ * 
+ */
+public class MapManager
+{
+    public List<Driver> drivers = new List<Driver>();
+    public List<Driver> waitingDrivers = new List<Driver>(); //Sorted descending, first events are last here.
+    public List<Driver> activeDrivers = new List<Driver>();
+    public MapManager()
+    {
+        //TODO: Argument: Filename? Stream? Decoded JSON? [could do this ourselves as well tbh]
+
+        //Load song and init [...]
+
+        //Iterate over all JSON driver entries,
+        //create new Drivers,
+        //store em all in a drivers list,
+        //sort by firstTimestamp (but so that the first elements stand last - lesser reordering!)
+    }
+
+    public void Update(float timestamp)
+    {
+        while (waitingDrivers[waitingDrivers.Count-1].InstantiateIfTime(timestamp))
+        {
+            int index = waitingDrivers.Count - 1;
+            activeDrivers.Add(waitingDrivers[index]);
+            waitingDrivers.RemoveAt(index);
+        }
+        int i = 0;
+        while (i <= activeDrivers.Count)
+        {
+            if (activeDrivers[i].Update(timestamp))
+            {
+                activeDrivers.RemoveAt(i);
+            } else
+            {
+                i++;
+            }
+        }
+    }
+
+    public void CleanUp()
+    {
+        foreach (Driver d in activeDrivers)
+        {
+            d.CleanUp();
+        }
+    }
+}
