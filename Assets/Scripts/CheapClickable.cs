0d27b151ded43be07673f4543a92860be22ed739
Ramin Kosfeld <r.kosfeld@tu-bs.de>
2021-12-25 23:40:52 +0100

    SaveState before I generate some input files from SteamVR

---
+++ Assets/Scripts/CheapClickable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CheapClickable : MonoBehaviour
+{
+    public UnityEvent onClick;
+
+    public void Click()
+    {
+        onClick.Invoke();
+    }
+
+    public void Hover()
+    {
+        transform.localScale = Vector3.one * 1.1f;
+    }
+
+    public void Unhover()
+    {
+        transform.localScale = Vector3.one;
+    }
+}
---
+++ Assets/Scripts/CheapPointer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheapPointer : MonoBehaviour
+{
+    protected CheapClickable hover;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit info;
+        Physics.Raycast(transform.position, transform.forward, out info);
+        CheapClickable click = info.transform.gameObject.GetComponent<CheapClickable>();
+        if (click != hover)
+        {
+            if (hover)
+            {
+                hover.Unhover();
+            }
+            if (click)
+            {
+                click.Hover();
+            }
+        }
+        if (SteamVR_Actions. && click) // Right control
+        {
+            click.Click();
+        }
+    }
+}
